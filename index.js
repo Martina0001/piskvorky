67956a79e24d5efc1eb22f3b8f557118057048c9
Martina Hejná <m.sejkova@gmail.com>
2021-04-16 23:10:16 +0200

    nahrání správné verze

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
+/** 
 'use strict';
 
 let player = 'circle';
 
-/***/
 
-const btnElm = document.querySelectorAll('.td');
 
-const imgElm = document.querySelector('.circle');
+const btnElm = document.querySelectorAll('.tabulka');
 
-/***/
+const imgElm = document.querySelector('.bunka');
+
+
 
 btnElm.forEach((table) => {
   table.addEventListener('click', (event) => {
     if (player === 'circle') {
-      button.classList.add('td--circle');
+      button.target.classList.add('td--circle');
       imgElm.src = 'images/cross.svg';
       player = 'cross';
-      button.disabled = true;
+      //  button.disabled = true;
     } else if (player === 'cross') {
-      button.classList.add('td--cross');
+      button.target.classList.add('td--cross');
       imgElm.src = 'images/circle.svg';
       player = 'circle';
-      button.disabled = true;
+      //  button.disabled = true;
     }
   });
+});
+*/
+
+'use strict';
+
+let playing = 'circle';
+
+let click = 0;
+const playingCross = document.querySelector('.circle');
+
+const playingFunction = (event) => {
+  if (event.target.disabled) {
+    return;
+  }
+  click++;
+  if (click % 2 != 0) {
+    event.target.classList.add('td--circle');
+    playingCross.src = 'images/cross.svg';
+  } else {
+    event.target.classList.add('td--cross');
+    playingCross.src = 'images/circle.svg';
+  }
+  event.target.disabled = true;
+};
+
+document.querySelectorAll('.tabulka button').forEach((element) => {
+  element.addEventListener('click', playingFunction);
 });
